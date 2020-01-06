c7b6f58344469f5295c4db223121eddab2c29c5a
Alexey Zolotykh <Alexey.Zolotykh@infobip.com>
2020-01-07 02:45:44 +0300

    Add joi config

--- packages/server/src/app.service.ts
+++ packages/server/src/app.service.ts
@@ -1,8 +1,12 @@
-import { Injectable } from '@nestjs/common';
+import {Injectable} from '@nestjs/common';
+import {ConfigService} from '@nestjs/config';
 
 @Injectable()
 export class AppService {
+  constructor(private configService: ConfigService) {}
+
   getHello(): string {
-    return 'Hello World!';
+    const config = this.configService.get('NODE_ENV');
+    return `Hello World! ${config}`;
   }
 }
