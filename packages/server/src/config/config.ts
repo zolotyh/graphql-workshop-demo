d625586473e5c11a4de55514e963a8183bc53558
Alexey Zolotykh <Alexey.Zolotykh@infobip.com>
2020-01-07 02:35:05 +0300

    Add Joi config support

--- packages/server/src/config/config.ts
+++ packages/server/src/config/config.ts
@@ -1,2 +1,15 @@
 import {ConfigModule} from '@nestjs/config';
-export const ConfigModuleForRoot = ConfigModule.forRoot();
+import * as Joi from '@hapi/joi';
+
+export const ConfigModuleForRoot = ConfigModule.forRoot({
+  validationSchema: Joi.object({
+    NODE_ENV: Joi.string()
+      .valid('development', 'production', 'test', 'provision')
+      .default('development'),
+    PORT: Joi.number().default(3000),
+  }),
+  validationOptions: {
+    allowUnknown: false,
+    abortEarly: true,
+  },
+});
