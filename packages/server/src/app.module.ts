f62747e3d3a996be56a778aa4b614d15be0e7e04
Alexey Zolotykh <Alexey.Zolotykh@infobip.com>
2020-01-07 03:56:11 +0300

    Add database module for nest

--- packages/server/src/app.module.ts
+++ packages/server/src/app.module.ts
@@ -2,9 +2,10 @@ import {Module} from '@nestjs/common';
 import {AppController} from './app.controller';
 import {AppService} from './app.service';
 import {GraphQLModuleForRoot, ConfigModuleForRoot} from 'src/config';
+import { DatabaseModule } from './database/database.module';
 
 @Module({
-  imports: [GraphQLModuleForRoot, ConfigModuleForRoot],
+  imports: [GraphQLModuleForRoot, ConfigModuleForRoot, DatabaseModule],
   controllers: [AppController],
   providers: [AppService],
 })
