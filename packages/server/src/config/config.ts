bd5f6a3a2ad2e66fd70c3b1c69f2edecbed8c5d0
Alexey Zolotykh <Alexey.Zolotykh@infobip.com>
2020-01-07 06:54:12 +0300

    Remove unecessary config

--- packages/server/src/config/config.ts
+++ packages/server/src/config/config.ts
@@ -3,16 +3,10 @@ import * as Joi from '@hapi/joi';
 
 export const ConfigModuleForRoot = ConfigModule.forRoot({
   validationSchema: Joi.object({
-    DB_HOST: Joi.string().required(),
-    DB_PORT: Joi.number().required(),
-    DB_TYPE: Joi.string().required(),
     NODE_ENV: Joi.string()
       .valid('development', 'production', 'test', 'provision')
       .default('development'),
-    PORT: Joi.number().default(5000),
-    POSTGRES_DB: Joi.string().required(),
-    POSTGRES_PASSWORD: Joi.string().required(),
-    POSTGRES_USER: Joi.string().required(),
+    PORT: Joi.number().default(3000),
   }),
   validationOptions: {
     allowUnknown: false,
